075fb7da378d7ad498d77a01f299207b70d64176
fruitschen <fruitschen@gmail.com>
2013-11-12 21:52:53 +0800

    exit when the command last longer than CRON_TIMEOUT, if it's set.

--- django_cron/management/commands/cronjobs.py
+++ django_cron/management/commands/cronjobs.py
@@ -2,12 +2,23 @@
 # run the cron service (intended to be executed from a cron job)
 #
 # usage: manage.py cronjobs
-
+import sys
+import signal
 from datetime import datetime
 
 from django.conf import settings
 from django.core.management.base import NoArgsCommand
 import django_cron
+
+# exit when the command last longer than CRON_TIMEOUT, if it's set.  
+if getattr(settings, 'CRON_TIMEOUT', False):
+    def timeout_check(signum, stack):
+        print 'Timeout, exiting!'
+        sys.exit()
+    
+    signal.signal(signal.SIGALRM, timeout_check)
+    signal.alarm(settings.CRON_TIMEOUT)
+
 
 class Command(NoArgsCommand):
     help = "run the cron services (intended to be executed from a cron job)"
