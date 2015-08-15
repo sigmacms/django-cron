fb6dee9b0ba824318a07ba1341d82d61daec709b
Andy Baker <andy@andybak.net>
2015-08-15 17:01:17 +0100

    use BASE_DIR or fallback to old PROJECT_DIR

--- django_cron/cron_settings.py
+++ django_cron/cron_settings.py
@@ -2,5 +2,6 @@ import os
 
 from django.conf import settings
 
-PID_FILE = getattr(settings, 'CRON_PID_FILE', os.path.join(settings.PROJECT_DIR, 'cron_pid.pid'))
+base_dir = getattr(settings, 'BASE_DIR') or getattr(settings, 'PROJECT_DIR')
+PID_FILE = getattr(settings, 'CRON_PID_FILE', os.path.join(base_dir, 'cron_pid.pid'))
 RETRY = getattr(settings, 'CRON_RETRY', False)
