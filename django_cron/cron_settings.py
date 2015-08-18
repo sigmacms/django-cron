8e3b46de0fc18b1d8271d5f40087e182b3da1e14
fruitschen <fruitschen@gmail.com>
2015-08-18 14:19:16 +0800

    add fallback value for base_dir setting, or it might raise errors

--- django_cron/cron_settings.py
+++ django_cron/cron_settings.py
@@ -2,6 +2,6 @@ import os
 
 from django.conf import settings
 
-base_dir = getattr(settings, 'BASE_DIR') or getattr(settings, 'PROJECT_DIR')
+base_dir = getattr(settings, 'BASE_DIR', '') or getattr(settings, 'PROJECT_DIR', '')
 PID_FILE = getattr(settings, 'CRON_PID_FILE', os.path.join(base_dir, 'cron_pid.pid'))
 RETRY = getattr(settings, 'CRON_RETRY', False)
